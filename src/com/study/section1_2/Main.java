3d8f6b19680df5be51a4c4319420e27a53b876f8
shsha4 <shsha4@naver.com>
2022-03-22 16:56:10 +0900

    특정 문자 뒤집기까지 완료

--- src/com/study/section1_2/Main.java
+++ src/com/study/section1_2/Main.java
@@ -1,5 +1,28 @@
 package com.study.section1_2;
 
-public class Main {
+import java.util.Scanner;
 
+/**
+ *  대소문자 변환
+ * */
+public class Main {
+    public String solution(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            /* Character 객체 안 isLowerCase 로 소문자 확인 */
+            if (Character.isLowerCase(x)) {
+                answer += Character.toUpperCase(x);
+            } else {
+                answer += Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
 }
