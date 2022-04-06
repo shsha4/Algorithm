36b3f1e87f5a4ba5f5fb7cf61df668dd6c6951cc
shsha4 <shsha4@naver.com>
2022-04-06 17:00:32 +0900

    등수 구하기까지 완료

---
+++ src/com/study/section2_7/Main.java
@@ -0,0 +1,33 @@
+package com.study.section2_7;
+
+import java.util.Scanner;
+
+/**
+ *  점수 계산
+ * */
+public class Main {
+    public int solution(int[] arr) {
+        int answer = 0;
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                cnt++;
+                answer += cnt;
+            } else {
+                cnt = 0;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(arr));
+    }
+}
