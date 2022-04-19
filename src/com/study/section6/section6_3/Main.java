e3053ad2f3af4bdcf671a69a7ed620cf5e473293
minjunpark <shsha4@naver.com>
2022-04-19 21:41:22 +0900

    삽입 정렬 까지 완료

---
+++ src/com/study/section6/section6_3/Main.java
@@ -0,0 +1,33 @@
+package com.study.section6.section6_3;
+
+import java.util.Scanner;
+
+/**
+ * 삽입 정렬
+ * N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ * 정렬하는 방법은 삽입정렬입니다.
+ * */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > temp) {
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j+1] = temp;
+        }
+        for (int x : arr) System.out.print(x + " ");
+    }
+}
